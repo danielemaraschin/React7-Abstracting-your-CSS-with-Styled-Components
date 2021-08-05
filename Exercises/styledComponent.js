57ee0a624b7c546a6d4e144c2fb3ea9059df9b2e
danielemaraschin <danikmaraschin@gmail.com>
2021-08-05 16:21:31 +0200

    exercise with styled.tagHTML

---
+++ Exercises/styledComponent.js
@@ -0,0 +1,10 @@
+const Botao = ({children})=>{
+    return(<button className="botao-geral">children</button>)
+  }
+
+  const Botao = styled.button`
+  background-color: #3f51b5;
+  color: white;
+  padding: 10px;
+  border: none;
+`;
