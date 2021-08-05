89707035579f6ba5f7ffd094b32871dc2e22b4bb
danielemaraschin <danikmaraschin@gmail.com>
2021-08-05 14:50:02 +0200

    Apply reset CSS with GlobalStyled

---
+++ src/Components/GlobalStyled.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+//podemos dar o reset css com o styled component tb, mas para isso precisamos importar a createGlobalStyle
+export const GlobalStyled =  createGlobalStyle`
+* {
+    box-sizing: border-box;
+    font-family: "Montserrat", sans-serif;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    color: grey;
+  }
+`; //aqui dentro da crase colocamos todo o reset do css que queremos dar RESET
+//Podemos recortar do arquivo do css
