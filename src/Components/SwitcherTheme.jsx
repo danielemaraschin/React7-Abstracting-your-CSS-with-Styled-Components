3bf6a005c220573f8bf1810141973c89e5f92425
danielemaraschin <danikmaraschin@gmail.com>
2021-08-12 07:44:33 +0200

    icone images

--- src/Components/SwitcherTheme.jsx
+++ src/Components/SwitcherTheme.jsx
@@ -1,7 +1,6 @@
-import { isContinueStatement } from "@babel/types";
 import React from "react";
-import ThemeOn from '../../asset/images/themeOn.svg'
-import ThemeOff from '../assets/images/ThemeOff.svg'
+import ThemeOn from '../assets/images/themeOn.svg'
+import ThemeOff from '../assets/images/themeOff.svg'
 import { Icone } from "./UI";
 
 const claro = <Icone src={ThemeOn} alt ="Tema Claro"/>
