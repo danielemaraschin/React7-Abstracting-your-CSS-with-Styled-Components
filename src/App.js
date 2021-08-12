6e392d3796780388eabd6db818728754b8b3e0a7
danielemaraschin <danikmaraschin@gmail.com>
2021-08-12 07:39:50 +0200

    Creating a button SwitcherTheme

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import { temaClaro, temaEscuro } from "./Components/UI/temas";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyled} from "./Components/GlobalStyled"
+import { BtnTema } from "./Components/UI";
+import SwitcherTheme from "./Components/SwitcherTheme";
 
 function App() {
   return (
     <ThemeProvider theme={temaEscuro}>
     <GlobalStyled/>
+    <BtnTema>
+      <SwitcherTheme></SwitcherTheme>
+    </BtnTema>
       <Cabecalho />
       <Container />
     </ThemeProvider>
